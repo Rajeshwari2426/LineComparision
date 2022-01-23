9332f0175396808a5f29492f44d869d7cae82e83
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-01-24 01:58:05 +0530

    adding lines length equality check program

--- LineComparision/LineComparision/Program.cs
+++ LineComparision/LineComparision/Program.cs
@@ -8,13 +8,16 @@ namespace LineComparision
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-         Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
+       static void Main(string[] args)
+       {
+          Console.WriteLine("Welcome to Line Comparison Computation Program on Master Branch");
             
          int X_One,X_Two;
          int Y_One, Y_Two;
-         
+         int S_One, S_Two;
+         int R_One , R_Two;
+
+
          Console.WriteLine("Enter x, y co ordinates of the line 1 :");
          Console.WriteLine("Enter value X_One");
          X_One = Convert.ToInt32(Console.ReadLine());
@@ -25,10 +28,31 @@ namespace LineComparision
          Console.WriteLine("Enter vale Y_Two");
          Y_Two = Convert.ToInt32(Console.ReadLine());
 
+         Console.WriteLine("Enter x, y co ordinates of the line 2 :");
+         Console.WriteLine("Enter value of S_One");
+         S_One = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Enter value of S_Two");
+         S_Two = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Enter vale R_One");
+         R_One = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Enter vale R_Two");
+         R_Two = Convert.ToInt32(Console.ReadLine());
+
          double Length_XY = Math.Sqrt(Math.Pow((X_One - X_Two), 2) + Math.Pow((Y_Two - Y_One), 2));
          Console.WriteLine("Length of Line 1 is:: " + Length_XY);
+           
+         double Length_SR = Math.Sqrt(Math.Pow((S_Two - S_One), 2) + Math.Pow((R_Two - R_One), 2));
+         Console.WriteLine("Length of Line 2 is: " + Length_SR);
+            
+         bool equal_result = Length_XY.Equals(Length_SR);
+                     
+         if (equal_result == false)
+                Console.WriteLine(" Length of XY and Length of SR are NOT Equal");
+         else
+                Console.WriteLine("Length of XY and Length of SR are Equal");
+         Console.ReadKey();
 
-         Console.ReadLine();        
+        Console.ReadLine();        
         }
     }
 }
